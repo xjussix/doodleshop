8b7c57657c286fa685cbf4aaa05c865fdb8fa078
Daniel Marell <daniel.marell@cag.se>
2014-06-11 09:31:11 +0200

    Added unit test.

---
+++ src/test/java/se/cag/doodleshop/TestApplicationContext.java
@@ -0,0 +1,55 @@
+/*
+ * Created by Daniel Marell 14-06-10 11:13
+ */
+package se.cag.doodleshop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@EnableJpaRepositories
+public class TestApplicationContext {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:mem.");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setJpaVendorAdapter(jpaVendorAdapter);
+        factory.setPackagesToScan("se.cag.doodleshop");
+        factory.setJpaProperties(getJpaProperties());
+        return factory;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabase(Database.HSQL);
+        return adapter;
+    }
+
+    private Properties getJpaProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.format_sql", "true");
+            }
+        };
+    }
+}
