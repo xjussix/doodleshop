a586b6cd43efcba4b3cf27c7980b1aa70a64fd97
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-25 17:56:45 +0200

    Now green, now removed the hardcoded name, but it will be red again util you update the pipeline

--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+INSTANS=$1
 PORT=8080
-IP=`docker inspect doodleshop-test | grep IPAddress | awk -F'\"' '{print $4}'`
+IP=`docker inspect ${INSTANS} | grep IPAddress | awk -F'\"' '{print $4}'`
 if ! `curl -f http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
 then
     echo "Test failed if ! `curl http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`"
