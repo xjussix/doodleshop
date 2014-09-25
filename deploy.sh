163ff14659b3df4d51b0000911e9a9a1952e1fd6
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-25 21:41:10 +0200

    Third try with new deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-INSTANS=$1
-VERSION=$2
-PORTPREFIX=$3
-echo running sudo docker rm -f--name doodleshop-${INSTANS}
-sudo docker rm -f--name doodleshop-${INSTANS}
+INSTANS=${DOODLE_ENV}
+VERSION=${GO_PIPELINE_LABEL}
+PORTPREFIX=$1
+echo running sudo docker rm -f doodleshop-${INSTANS}
+sudo docker rm -f doodleshop-${INSTANS}
 echo running sudo docker run -d --name doodleshop-${INSTANS} -p ${PORTPREFIX}8080:8080 doodleshop:$VERSION
 sudo docker run -d --name doodleshop-${INSTANS} -p ${PORTPREFIX}8080:8080 doodleshop:$VERSION
 
