355e8a431d5e9292dc3ee7721937e7c767b82920
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-25 20:30:26 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 INSTANS=$1
 VERSION=$2
 PORTPREFIX=$3
-
-sudo docker rm -d --name doodleshop-${INSTANS}
+echo running sudo docker rm -f--name doodleshop-${INSTANS}
+sudo docker rm -f--name doodleshop-${INSTANS}
+echo running sudo docker run -d --name doodleshop-${INSTANS} -p ${PORTPREFIX}8080:8080 doodleshop:$VERSION
 sudo docker run -d --name doodleshop-${INSTANS} -p ${PORTPREFIX}8080:8080 doodleshop:$VERSION
 
