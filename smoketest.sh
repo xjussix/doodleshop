2dfb402bd699691e8b4bec6559fe6ce98ea0c531
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-25 17:45:32 +0200

    Update how the smoketest find the right docker instans, currently hard coded

--- smoketest.sh
+++ smoketest.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PORT=$1
-if ! `curl -f http://localhost:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
+PORT=8080
+IP=`docker inspect doodleshop-test | grep IPAddress | awk -F'\"' '{print $4}'`
+if ! `curl -f http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
 then
-    echo "Test failed if ! `curl http://localhost:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`"
+    echo "Test failed if ! `curl http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`"
     exit 1
 fi
 # More tests here!
