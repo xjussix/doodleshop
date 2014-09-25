5409f234dadf0eb76113d7561ce95a3b0c7f9826
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-25 21:58:44 +0200

    New smoketest, wish looks at env

--- smoketest.sh
+++ smoketest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-INSTANS=$1
+INSTANS=$DOODLE_ENV
 PORT=8080
-IP=`docker inspect ${INSTANS} | grep IPAddress | awk -F'\"' '{print $4}'`
+IP=`docker inspect doodleshop-${INSTANS} | grep IPAddress | awk -F'\"' '{print $4}'`
 if ! `curl -f http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
 then
-    echo "Test failed if ! `curl http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`"
+    echo "Test failed (curl http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null)"
     exit 1
 fi
 # More tests here!
