c41017e7958057a0062905c76a36456fe6780cb0
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-26 22:09:17 +0200

    #1 Nu utan stderr också

--- runsmoketestuntilkilled.sh
+++ runsmoketestuntilkilled.sh
@@ -3,7 +3,7 @@
 while `/bin/true`
 do
     echo try to run smoketest.sh
-    if `bash smoketest.sh >/dev/null`; then break; fi
+    if `bash smoketest.sh >/dev/null 2>&1`; then break; fi
     sleep 3
 done
 
