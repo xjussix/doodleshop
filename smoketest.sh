b9f08614fd5872af9d1cf24e24e7360a20a1d2bd
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-26 17:20:59 +0200

    Ändrade variabelnamn och lite kommentarer

--- smoketest.sh
+++ smoketest.sh
@@ -2,10 +2,13 @@
 
 INSTANS=$DOODLE_ENV
 PORT=8080
-IP=`docker inspect doodleshop-${INSTANS} | grep IPAddress | awk -F'\"' '{print $4}'`
-if ! `curl -f http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
+
+# Get the IPAdress of the docker instans
+DOODLE_IP=`docker inspect doodleshop-${INSTANS} | grep IPAddress | awk -F'\"' '{print $4}'`
+# Basic smoketest, TODO: should be changed to environment?
+if ! `curl -f http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
 then
-    echo "Test failed (curl http://${IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null)"
+    echo "Test failed (curl http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null)"
     exit 1
 fi
 # More tests here!
