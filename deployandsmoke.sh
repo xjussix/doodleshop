9d393a18afdc74d33d5670b4ed274b1bc1a82fac
Mikael Sennerholm <mikael@sennerholm.net>
2014-09-26 20:43:58 +0200

    Sleep 30 before running smoketests

--- deployandsmoke.sh
+++ deployandsmoke.sh
@@ -13,6 +13,6 @@ sudo docker rm -f doodleshop-${INSTANS} || /bin/true
 echo running sudo docker run -d -e "DOODLE_ENV=${DOODLE_ENV}" --name doodleshop-${INSTANS} -p ${PORTPREFIX}${PORT}:${PORT} doodleshop:$VERSION
 sudo docker run -d -e "DOODLE_ENV=${DOODLE_ENV}" --name doodleshop-${INSTANS} -p ${PORTPREFIX}${PORT}:${PORT} doodleshop:$VERSION
 # Sleep some time before checking that it's working
-sleep 10
+sleep 30
 bash smoketest.sh 
 
