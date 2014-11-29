1d73ed2c09c67822871803cac76e8803bce9f617
Mikael Sennerholm <mikael@sennerholm.net>
2014-11-29 22:10:43 +0100

    Hopefully working

--- scripts/deployandsmoke.sh
+++ scripts/deployandsmoke.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Deploy and run smoketest on doodle
 INSTANCE=${DOODLE_ENV}
-VERSION=${GO_PIPELINE_LABEL}
 PORT_PREFIX=$1
 PORT=8080
 
 set -e
+# Newversion
+#VERSION=${GO_PIPELINE_LABEL} (Old, may not always working futher down in the dependency chain)
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SCRIPT_DIR=$(dirname $(readlink -f $0))"
+VERSION=`grep AppVersion ${SCRIPT_DIR}/../BuildInfo.propertiesproperties  | awk -F'=' '{print $2}'`
+echo "Deploy Version ${VERSION}"
 # Kill old instans
 echo running sudo docker rm -f doodleshop-${INSTANCE}
 sudo docker rm -f doodleshop-${INSTANCE} || /bin/true
